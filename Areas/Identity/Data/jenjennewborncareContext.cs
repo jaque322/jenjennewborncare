0b413e1e814e83a26e7b35070379b0828fd52b1d
jansel jaque <janselsobrino@gmail.com>
2023-04-21 19:11:27 -0400

    dbcontext updated

---
+++ Areas/Identity/Data/jenjennewborncareContext.cs
@@ -0,0 +1,30 @@
+﻿using jenjennewborncare.Areas.Identity.Data;
+using jenjennewborncare.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace jenjennewborncare.Data;
+
+public class jenjennewborncareContext : IdentityDbContext<User>
+{
+
+    public DbSet<Review> review { get; set; }
+    public DbSet<BabyCareServiceModel> BabyCareServices { get; set; }
+    public DbSet<ServiceImage> ServiceImages { get; set; }
+
+    public jenjennewborncareContext(DbContextOptions<jenjennewborncareContext> options)
+        : base(options)
+    {
+    }
+
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using jenjennewborncare.Data;
+using jenjennewborncare.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("jenjennewborncareContextConnection") ?? throw new InvalidOperationException("Connection string 'jenjennewborncareContextConnection' not found.");
+var connectionString = "server=localhost;database=jenjeare_main;uid=jenje_01;pwd=?a5F7ds71";
+builder.Services.AddDbContext<jenjennewborncareContext>(options =>
+    options.UseMySQL(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<jenjennewborncareContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,9 +25,10 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
